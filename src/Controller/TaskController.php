467b85cf2cb71db21ee4799c7784f558244dc523
Meviarie <bacia558@vp.pl>
2019-03-21 20:00:50 +0100

    Commit command

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -1,54 +1,72 @@
 <?php
+/**
+ * Task controller.
+ */
 
 namespace App\Controller;
 
-
+use App\Entity\Task;
 use App\Repository\TaskRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 /**
- *
- * Class TaskController
+ * Class TaskController.
  *
  * @Route("/task")
- *
  */
-
 class TaskController extends AbstractController
 {
     /**
-     * @return Response
+     * Index action.
      *
-     * @Route("/", name="task_index")
+     * @param \Symfony\Component\HttpFoundation\Request $request    HTTP request
+     * @param \App\Repository\TaskRepository            $repository Repository
+     * @param \Knp\Component\Pager\PaginatorInterface   $paginator  Paginator
+     *
+     * @return \Symfony\Component\HttpFoundation\Response HTTP response
+     *
+     * @Route(
+     *     "/",
+     *     name="task_index",
+     * )
      */
-
-    public function index(TaskRepository $taskRepository): Response
+    public function index(Request $request, TaskRepository $repository, PaginatorInterface $paginator): Response
     {
+        $pagination = $paginator->paginate(
+            $repository->queryAll(),
+            $request->query->getInt('page', 1),
+            Task::NUMBER_OF_ITEMS
+        );
+
         return $this->render(
             'task/index.html.twig',
-            ['tasks' => $taskRepository->findAll()]
+            ['pagination' => $pagination]
         );
     }
 
-
     /**
+     * View action.
      *
-     * @param TaskRepository $taskRepository
-     * @param Task $task
-     * @return Response
+     * @param \App\Entity\Task $task Task entity
+     *
+     * @return \Symfony\Component\HttpFoundation\Response HTTP response
      *
      * @Route(
      *     "/{id}",
      *     name="task_view",
-     *     )
+     *     requirements={"id": "[1-9]\d*"},
+     * )
      */
-
     public function view(Task $task): Response
     {
         return $this->render(
-            '',['task'=>$task]
+            'task/view.html.twig',
+            ['task' => $task]
         );
     }
 }
